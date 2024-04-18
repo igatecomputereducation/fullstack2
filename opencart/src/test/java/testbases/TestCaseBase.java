ce3252bd9b5840d37207147a559f108590ee2336
igatecomputereducation <igate computereducation@gmail.com>
2024-04-18 16:37:52 +0530

    v1

--- opencart/src/test/java/testbases/TestCaseBase.java
+++ opencart/src/test/java/testbases/TestCaseBase.java
@@ -1,22 +1,38 @@
 package testbases;
-
+import java.io.FileReader;
 import java.time.Duration;
-
+import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 
+import com.github.javafaker.Faker;
+
 public class TestCaseBase {
 
 	protected WebDriver driver;
-	
+	protected Logger log;
+	protected Faker faker; 
+	protected Properties p;
 	@BeforeClass
 	public void setUp() {
+		log = LogManager.getLogger();
+		faker = new Faker();
+		try {
+			FileReader fread=new FileReader(".//src/test/resources/data.properties");
+			p=new Properties();
+			p.load(fread);
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e.getMessage());
+		}
 		driver=new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(5));
-		driver.get("http://localhost:8080/opencart/upload/");
+		driver.get(p.getProperty("openpage"));
 		driver.manage().window().maximize();
 	}
-	
 }
--- opencart/src/test/java/testcases/TC_01_Register.java
+++ opencart/src/test/java/testcases/TC_01_Register.java
@@ -10,6 +10,7 @@ public class TC_01_Register extends TestCaseBase{
 		
 	@Test(priority = 1)
 	public void register() {
+		log.info("***Beg of TC_01_register*****");
 		HomePage hp=new HomePage(driver);
 		hp.registerClick();
 		Assert.assertTrue(hp.isRegisterLoaded());
@@ -17,18 +18,17 @@ public class TC_01_Register extends TestCaseBase{
 	
 	@Test(priority = 2,dependsOnMethods = {"register"})
 	public void registerDataentry() {
+		log.info("Entering register data..");
+		String fkpwd=faker.internet().password();
 		RegisterPage rp=new RegisterPage(driver);
-		rp.setFirstName("subbu");
-		rp.setLastName("immadisetty");
-		rp.setEmail("hello@gmail.com");
-		rp.setTelephone("9234474");
-		rp.setPassword("password123");
-		rp.setConfirm("password123");
+		rp.setFirstName(faker.address().firstName());
+		rp.setLastName(faker.address().lastName());
+		rp.setEmail(faker.internet().emailAddress());
+		rp.setTelephone(faker.phoneNumber().cellPhone());
+		rp.setPassword(fkpwd);
+		rp.setConfirm(fkpwd);
 		rp.setNewsLetter("yes");
 		rp.clickAgree();
 		rp.clickContinue();
-		
 	}
-	
-	
 }
